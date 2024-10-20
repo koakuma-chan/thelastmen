73859f3797c99706402a1e3b076b93a10e4575cc
koakuma-chan <fresh.salt0811@fastmail.com>
2024-10-20 12:25:38 -0400

    Implement pagination

--- website/src/articles.ts
+++ website/src/articles.ts
@@ -1,61 +1,51 @@
-import postgres from "postgres";
+export async function fetchArticleCount(
+  //
+  sql: any,
+): Promise<number> {
+  const res = await sql`select count(*) as count from articles`;
 
-export async function fetchArticles(env: any): Promise<{ slug: string; title: string; thumbnailUrl: string }[]> {
-  const sql = postgres({
-    host: env.DB_HOST,
-    port: parseInt(env.DB_PORT, 10),
-    username: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_DATABASE,
-  });
-
-  try {
-    const res = await sql`
-      select slug, title, thumbnail_url
-      from articles
-      order by time_created desc
-      limit 24
-    `;
-
-    return res.map(row => ({
-      slug: row.slug,
-      title: row.title,
-      thumbnailUrl: row.thumbnail_url,
-    }));
-  } catch (err) {
-    console.error("Error fetching articles:", err);
-    throw err;
-  }
+  return res[0].count;
 }
 
-export async function fetchArticleBySlug(env: any, slug: string): Promise<{ title: string; content: string }> {
-  const sql = postgres({
-    host: env.DB_HOST,
-    port: parseInt(env.DB_PORT, 10),
-    username: env.DB_USER,
-    password: env.DB_PASSWORD,
-    database: env.DB_DATABASE,
-  });
+export async function fetchArticles(
+  //
+  sql: any,
+  //
+  page: number,
+  //
+  pageSize: number,
+): Promise<{ slug: string; title: string; thumbnailUrl: string }[]> {
+  const res = await sql`
+    select slug, title, thumbnail_url
+    from articles
+    order by time_created desc
+    offset ${page * pageSize} limit ${pageSize}
+  `;
 
-  try {
-    const res = await sql`
-      select title, content
-      from articles
-      where slug = ${slug}
-    `;
+  return res.map(row => ({
+    slug: row.slug,
+    title: row.title,
+    thumbnailUrl: row.thumbnail_url,
+  }));
+}
 
-    if (res.length === 0) {
-      throw new Error(`Article with slug "${slug}" not found`);
-    }
+export async function fetchArticleBySlug(
+  //
+  sql: any,
+  //
+  slug: string,
+): Promise<{ title: string; content: string }> {
+  const res = await sql`
+    select title, content
+    from articles
+    where slug = ${slug}
+  `;
 
-    const row = res[0];
-
-    return {
-      title: row.title,
-      content: row.content,
-    };
-  } catch (err) {
-    console.error("Error fetching article by slug:", err);
-    throw err;
+  if (res.length === 0) {
+    throw new Error(`Article with slug "${slug}" not found`);
   }
+
+  const row = res[0];
+
+  return { title: row.title, content: row.content };
 }
